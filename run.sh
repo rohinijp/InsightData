dc66f002307330e993ef56edbb7d019e55b56b69
Rohini Jayaram <rohinijayaram@Rohinis-MacBook-Air.local>
2017-12-18 23:08:13 -0800

    HHHH

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # one example of run.sh script for implementing the features using python
 # the contents of this script could be replaced with similar files from any major language
 
-
+sss
 # I'll execute my programs, with the input directory log_input and output the files in the directory log_output
 set -x
 g++ -g -std=c++11 -o insight ./src/insight.cpp
